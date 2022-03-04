14801c1e8514f986cf59bf1a33cf8fc3ddf13945
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-04 22:28:13 +0300

    fix(ui): arrowhead width on Android

--- components/Dungeon/Arrow/Arrowhead.tsx
+++ components/Dungeon/Arrow/Arrowhead.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Defs, Marker, Path } from 'react-native-svg/src'
+import { Defs, Marker, Path } from 'react-native-svg'
 
 export function Arrowhead() {
   return (
     <Defs>
-      <Marker id="arrowOutline" markerWidth="1" markerHeight="1" refX=".75" refY="1" orient="auto">
+      <Marker viewBox="0 0 3 3" id="arrowOutline" refX=".75" refY="1" orient="auto">
         {/* @ts-ignore */}
         <Path d="M0,0 L0,2 L2,1" fill="black" pointerEvents={'none'} />
       </Marker>
-      <Marker id="arrowInline" markerWidth="1" markerHeight="1" refX="2" refY="2" orient="auto">
+      <Marker viewBox="0 0 3 3" id="arrowInline" refX="2" refY="2" orient="auto">
         {/* @ts-ignore */}
         <Path d="M0.9,0.8 L0.9,3.2 L3.5,2" fill="green" pointerEvents={'none'} />
       </Marker>
