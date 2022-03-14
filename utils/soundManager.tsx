944d2a51ea7ef1c198adb5e97b1d115277c72ac2
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-03-14 20:39:23 +0300

    refactor: optimize soundManager function creation

--- utils/soundManager.tsx
+++ utils/soundManager.tsx
@@ -1,15 +1,17 @@
-import { Audio, AVPlaybackNativeSource } from 'expo-av'
+import { Audio, AVPlaybackNativeSource, AVPlaybackStatus } from 'expo-av'
+import { Sound } from 'expo-av/build/Audio/Sound'
+
+const options = { shouldPlay: true }
+
+function errorCatcher() {}
+
+function soundUnloader(res: { sound: Sound, status: AVPlaybackStatus }) {
+  res.sound.setOnPlaybackStatusUpdate((status) => {
+    if(status.isLoaded && !status.didJustFinish) return
+    res.sound.unloadAsync().catch(errorCatcher)
+  })
+}
 
 export function playSound(sound: AVPlaybackNativeSource) {
-  Audio.Sound.createAsync(
-    sound,
-    { shouldPlay: true }
-  ).then((res)=>{
-    res.sound.setOnPlaybackStatusUpdate((status)=>{
-      if(status.isLoaded && !status.didJustFinish) return
-      res.sound.unloadAsync().catch(()=>{})
-    })
-  }).catch((error)=>{
-    console.log(error)
-  })
+  return Audio.Sound.createAsync(sound, options).then(soundUnloader).catch(errorCatcher)
 }
